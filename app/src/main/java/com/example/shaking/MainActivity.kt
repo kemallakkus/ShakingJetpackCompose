bae8ee48707b45c90efdb7fca0df6aa1c8546429
kemallakkus <kemalllakkus@gmail.com>
2024-06-28 14:38:48 +0300

    commit

--- app/src/main/java/com/example/shaking/MainActivity.kt
+++ app/src/main/java/com/example/shaking/MainActivity.kt
@@ -6,15 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.shaking.ui.ShakingState
 import com.example.shaking.ui.rememberShakingState
